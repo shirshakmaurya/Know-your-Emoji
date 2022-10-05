8e4d22dcdf7a8176161b8b6bc2f3f471d4318aee
shirshak <84394640+shirshakmaurya@users.noreply.github.com>
2022-10-05 14:52:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "🦍": "Gorilla",
+  "🦝": "Raccoon",
+  "🐹": "Hamster",
+  "🐿️": "Chipmunk",
+  "🕊️": "Dove",
+  "🦖":
+    "T-Rex" /** add some more to show how the app now expands when there's new data */
+};
+
+const emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emoji, setEmoji] = useState(""); /** concept 2 is useState */
+  const [meaning, setMeaning] = useState("translation will appear here..");
+
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+
+    if (inputEmoji in emojiDictionary) {
+      setMeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setMeaning("failure to recognise this emoji");
+    }
+  }
+
+  function emojiClickHandler(inputEmoji) {
+    setMeaning(emojiDictionary[inputEmoji]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Know Animal Emoji</h1>
+      <input
+        onChange={changeHandler}
+        value={emoji}
+        placeholder={"Search your animal"}
+        style={{
+          padding: "1em",
+          minWidth: "80%"
+        }}
+      />
+      <h2> {emoji} </h2>
+      <h3> {meaning} </h3>
+      {emojis.map((emoji) => (
+        <span
+          onClick={() => emojiClickHandler(emoji)}
+          style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+        >
+          {" "}
+          {emoji}{" "}
+        </span>
+      ))}
+    </div>
+  );
+}
